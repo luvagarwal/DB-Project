c39491ba1c1c48162f400296e0baf1b7492d844d
luviiit <luvagarwal1995su@gmail.com>
2014-10-31 23:53:09 +0530

    Added layout of all the pages, now _ trailing files need to be changed

---
+++ delievery.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php
+    session_start();
+    if(!isset($_SESSION['username'])){
+        header('Location: login.php');
+    }
+?>
+<html lang="en">
+    <head>
+        <title>Appolo-Home</title>
+        <?php include 'includes.php';?>
+    </head>
+    <body>
+        <?php include 'header.php';?>
+        <?php include '_delievery.php';?>
+        <?php
+            include 'footer.php';
+            include 'includesjs.php';
+        ?>
+    </body>
+</html>
--- includes.php
+++ includes.php
@@ -1,1 +1,14 @@
-<link rel="stylesheet" href="static/css/bootstrap.min.css" type="text/css">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Bootstrap Core CSS -->
+<link href="static/css/bootstrap.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="static/css/sb-admin.css" rel="stylesheet">
+
+<!-- Custom Fonts -->
+<link href="static/font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
