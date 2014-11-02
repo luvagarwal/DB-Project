37e3cdb0d0be47790c792b0fbd6ee3b5e63ceb69
Dhrumil <dhrumil2910@gmail.com>
2014-11-02 03:50:32 +0000

    dummy file

---
+++ dummyinsertdb.sql
@@ -0,0 +1,9 @@
+INSERT INTO HOSPITAL
+(hospital_id,hospital_name,hospital_type,address)
+VALUES
+(1,"Sarva Hospital","Cancer","A-131 dsdjsjda dadka dakd akdj");
+
+INSERT INTO DOCTOR
+(doctor_id,first_name,last_name,phone_no,address,hospital_id,sex,qualification)
+VALUES
+(1,"Amit","Shah","1234567890","A-31 djksaj ajsdkjaks jasd","1","M","MBBS,MBA");
