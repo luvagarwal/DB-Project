74c5dc3a8bb909a0ea2de885b033463a397b68b1
Dhrumil <dhrumil2910@gmail.com>
2014-11-02 14:12:50 +0000

    Doctors script

--- _doctors.php
+++ _doctors.php
@@ -1,3 +1,12 @@
+<br>
+<br>
+<br>
+<hr>
+<h2 style="color:#095d58;text-align:center"> The Doctors  Prescribed by the Customers </h2>
+<hr>
+<br>
+<br>
+<br>
 <?php
 // REQUIRED VARIABLES FOR CONNECTION
 
@@ -21,11 +30,11 @@ if(! $query )
   die('Could not enter data: ' . mysql_error());
 }
 ?>
-<table class='table table-hover'><tr><td>FIRST NAME</td><td>LAST NAME</td><td>PHONE NO</td><td>address</td></tr>
+<table class='table table-hover'><tr style="color:#0087C3;text-align:center"><td>FIRST NAME</td><td>LAST NAME</td><td>PHONE NO</td><td>address</td></tr>
 <?php
 while($row = mysql_fetch_array($query))
 {
-	echo "<tr><td>".$row['first_name']."</td>";
+	echo "<tr style='text-align:center'><td>".$row['first_name']."</td>";
 	echo "<td>".$row['last_name']."</td>";
 	echo "<td>".$row['phone_no']."</td>";
 	echo "<td>".$row['address']."</td></tr>";
