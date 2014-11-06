bfa94cae3e2fa7957c9d84d5eb57810bb5d60292
Dhrumil <dhrumil2910@gmail.com>
2014-11-06 20:42:13 +0000

    modification in employee view

---
+++ editemp.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>Edit Employee</title>
+        <?php include 'includes.php';?>
+    </head>
+    <body>
+        <?php include 'header.php';?>
+      
+  		<?php
+			$conn = mysql_connect("localhost","root","123123") or die(mysql_error());
+			mysql_select_db("APOLLO",$conn) or die(mysql_error());
+			if(isset($_GET['employee_id'])){
+				$query = "Select * from EMPLOYEE WHERE employee_id=".$_GET['employee_id'];
+				$results = mysql_query($query);
+				if(!$results)
+				{
+					die('Could not enter data: ' . mysql_error());
+				}
+				$row = mysql_fetch_array($results);	
+			}
+			if(isset($_POST['submit']))
+			{
+					$user_name = $_POST['user_name'];
+					$first_name = $_POST['first_name'];
+					$last_name = $_POST['last_name'];
+					$date = $_POST['date'];
+					$sex= $_POST['sex'];
+					$date_joining = $_POST['date_joining'];
+					$address = $_POST['address'];
+					$salary = $_POST['salary'];
+					$emp_type = $_POST['emp_type'];
+					$phone_no = $_POST['phone_no'];
+					$qualify = $_POST['qualify'];
+					$file = $_FILES['image']['tmp_name'];
+					$password = md5($_POST['password']);
+					if(!isset($file))
+					{
+						echo "Please enter a file";
+					}
+					else
+					{
+						$imgData = addslashes(file_get_contents($_FILES['image']['tmp_name']));
+						$image_name = addslashes($_FILES['image']['name']);
+						$imageProperties = getimagesize($_FILES['image']['tmp_name']);
+						if($imageProperties == FALSE)
+							echo "This is not an Image";
+						else
+						{
+							if(!$insert = mysql_query("UPDATE EMPLOYEE SET user_name=$user_name',first_name='$first_name',last_name='$last_name',imageType='{$imageProperties['mime']}',imageData='{$imgData}',dob='$date',sex='$sex',date_of_joining='$date_joining',address='$address',salary='$salary',employee_type='$emp_type',phone_no='$phone_no',qualification = '$qualify',password='$password',store_id=1 where employee_id=".$_GET['employee_id']))
+							{
+								echo "Problem entering the data";
+							}
+						}
+					}
+mysql_close($conn);
+			}
+			
+
+
+		?>
+	  <br>
+      <br>
+      <br>
+<div id="form" style="width:50em;background-color:#f0f0f0;margin-left:16em;padding-top: 25px;padding-right: 25px ; padding-bottom: 25px;
+    padding-left: 25px;border: 0px solid;box-shadow: 5px 5px 5px 2px #888888">
+<form method="post" action="" enctype="multipart/form-data">
+
+    <div class="form-group">
+      <h5 style="color:#095d58">First Name:</h5>
+     <input type="text"class="form-control" name="first_name" id="first_name" style="width:30em" required value="<?php echo $row['first_name']; ?>">
+    </div>
+    <div class="form-group">
+      <h5 style="color:#095d58">Last Name:</h5>
+      <input type="text" class="form-control" name="last_name" id="Last_Name" style="width:30em " required value="<?php echo $row['last_name']; ?>">
+    </div>
+   <div class="form-group">
+				<div class='input-group date' id='datetimepicker5'>
+					<h5 style="color:#095d58"> Date of Birth: </h5>
+					<input type='text' class="form-control" data-date-format="YYYY/MM/DD" name="date" required value="<?php echo $row['dob']; ?>">
+					<span class="input-group-addon">
+						<span class="glyphicon glyphicon-calendar"></span>
+					</span>
+				</div>
+			</div>
+    <div class="form-group">
+      <h5 style="color:#095d58">Sex:</h5>
+      <input type="radio" name="sex" value="<?php echo $row['first_name']; ?>" checked> M<br>
+		<input type="radio" name="sex" value > F<br>
+    </div>
+    <div class="form-group">
+      <h5 style="color:#095d58">Date of Joining:</h5>
+      <input type="text"  class="form-control" name="date_joining" id="date_joining" style="width:30em " required value="<?php echo $row['date_of_joining']; ?>">
+    </div>
+
+    <div class="form-group">
+      <h5 style="color:#095d58">Address of Residence:</h5>
+      <textarea type="text"  rows="6"  cols="50" class="form-control" name="address" id="address" style="width:30em " required value="<?php echo $row['address']; ?>"></textarea> 
+    </div>
+
+
+    <div class="form-group">
+      <h5 style="color:#095d58">Salary:</h5>
+      <input type="text"  class="form-control" name="salary" id="salary" style="width:30em " required value="<?php echo $row['salary']; ?>"> 
+    </div>
+
+   <div class="form-group">
+      <h5 style="color:#095d58">Sex:</h5>
+      <input type="radio" name="emp_type" value="m" checked> Manager<br>
+		<input type="radio" name="emp_type" value="a" > Admin<br>
+		<input type="radio" name="emp_type" value="e" >Employee<br>
+    </div>
+
+    <div class="form-group">
+      <h5 style="color:#095d58">Phone No:</h5>
+      <input type="text"  class="form-control" name="phone_no" id="phone_no" style="width:30em " required value="<?php echo $row['phone_no']; ?>"> 
+    </div>
+
+    <div class="form-group">
+      <h5 style="color:#095d58">Qualifiations:</h5>
+      <input type="text"  class="form-control" name="qualify" id="qualify" style="width:30em " required value="<?php echo $row['qualification']; ?>"> 
+    </div>
+
+
+    <div class="form-group">
+      <h5 style="color:#095d58">Photo:</h5>
+      <input type="file"  name="image" id="image" required> 
+    </div>
+    <br>
+
+    <div class="form-group">
+      <h4 style="color:grey">Login Credentials</h4>
+    </div>
+
+
+    <div class="form-group">
+      <h5 style="color:#095d58">User Name:</h5>
+     <input type="text" class="form-control" onkeyup=checkusername(this.value) name="user_name" id="user_name" style="width:30em" required value="<?php echo $row['user_name']; ?>"> 
+    </div>
+    <span id="available" style="display: none; color: green;"class="glyphicon glyphicon-ok"></span>
+    <span id="notavailable" style="display: none; color: red"class="glyphicon glyphicon-remove"></span>
+    <div class="form-group">
+      <h5 style="color:#095d58">Password:</h5>
+      <input type="password" class="form-control" name="password" id="password" style="width:30em " required> 
+    </div>
+    <div class="form-group">
+      <h5 style="color:#095d58">Confirm Password:</h5>
+      <input type="password" class="form-control" name="confirm_password" id="confirm_password" style="width:30em " required> 
+    </div>
+
+    <button type="submit" class="btn btn-default" name="submit">Submit</button>
+  </form>
+</div>
+
+
+
+
+        <?php
+            include 'footer.php';
+            include 'includesjs.php';
+        ?>
+    </body>
+</html>
--- searchemp.php
+++ searchemp.php
@@ -31,20 +31,24 @@ if(empty($_GET)){
 	while($row = mysql_fetch_array($results))
 	{
 
-		echo "<div id='".$row['user_name']."' style='box-shadow: 1px 2px 2px 1px #888888;width:60em;background-color:#f0f0f0;margin-left:10em;height:20em;padding-right:20em;padding-top:2em'>";
+		echo "<div id='".$row['user_name']."' style='box-shadow: 1px 2px 2px 1px #888888;width:77em;background-color:#f0f0f0;margin-left:2em;height:30em;padding-right:20em;padding-top:2em'>";
+		
 		echo "<input type='submit' class='btn btn-default' value='Delete' style='float:right;margin-right:-19em' onclick=deleteemp('".$row['user_name']."')>";
+		echo "<a href='editemp.php?employee_id=".$row['employee_id']."'><span class='glyphicon glyphicon-pencil' style='float:right;margin-right:-13em;margin-top:0.5em' >Edit</span></a>";
+
 		echo "<img src='imageView.php?image_id=" .$row['employee_id']. "' style='width:12em;height:15em;float:left;margin-top:-0.5em'/>";
-		echo "<ul style='margin-left:25em;float:right;margin-left:-70em; list-style-type: none'><li><b style='color:#0000ff'>User Name:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</b>".$row['user_name']."<li>";
-		echo "<li><b style='color:#0000ff'>First Name:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</b>".$row['first_name']."</li>";
-		echo "<li><b style='color:#0000ff'>Last Name:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</b>".$row['last_name']."</li>";
-		echo "<li><b style='color:#0000ff'>Date of Birth:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</b>".$row['dob']."</li>";
-		echo "<li><b style='color:#0000ff'>Sex:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</b>".$row['sex']."</li>";
-		echo "<li><b style='color:#0000ff'>Date of Joining:&nbsp&nbsp&nbsp</b>".$row['date_of_joining']."</li>";
-		echo "<li><b style='color:#0000ff'>Address:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</b>".$row['address']."</li>";
-		echo "<li><b style='color:#0000ff'>Salary:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</b>".$row['salary']."</li>";
-		echo "<li><b style='color:#0000ff'>Employee Type:&nbsp&nbsp&nbsp</b>".$row['employee_type']."</li>";
-		echo "<li><b style='color:#0000ff'>Phone No:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</b>".$row['phone_no']."</li>";
-		echo "<li><b style='color:#0000ff'>Qualification:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</b>".$row['qualification']."</li></ul>";
+		echo "<table class='table table-hover' style='width:40em;float:right;margin-left:70em;margin-top:-17em; list-style-type: none'><tr><td colspan='2'><b style='color:#0000ff'>User Name:</b></td><td>".$row['user_name']."</td></tr>";
+		echo "<tr><td colspan='2'><b style='color:#0000ff'>First Name:&nbsp&nbsp&nbsp&nbsp&nbsp</b></td><td>".$row['first_name']."</td></tr>";
+		echo "<tr><td colspan='2'><b style='color:#0000ff'>Last Name:&nbsp&nbsp&nbsp&nbsp&nbsp</b></td><td>".$row['last_name']."</td></tr>";
+		echo "<tr><td colspan='2'><b style='color:#0000ff'>Date of Birth:&nbsp&nbsp&nbsp&nbsp&nbsp</b></td><td>".$row['dob']."</td></tr>";
+		echo "<tr><td colspan='2'><b style='color:#0000ff'>Sex:&nbsp&nbsp&nbsp&nbsp&nbsp</b></td><td>".$row['sex']."</td></tr>";
+		echo "<tr><td colspan='2'><b style='color:#0000ff'>Date Of Joining:&nbsp&nbsp&nbsp&nbsp&nbsp</b></td><td>".$row['date_of_joining']."</td></tr>";
+		echo "<tr><td colspan='2'><b style='color:#0000ff'>Address:&nbsp&nbsp&nbsp&nbsp&nbsp</b></td><td>".$row['address']."</td></tr>";
+		echo "<tr><td colspan='2'><b style='color:#0000ff'>Salary:&nbsp&nbsp&nbsp&nbsp&nbsp</b></td><td>".$row['salary']."</td></tr>";
+		echo "<tr><td colspan='2'><b style='color:#0000ff'>Employee Type:&nbsp&nbsp&nbsp&nbsp&nbsp</b></td><td>".$row['employee_type']."</td></tr>";
+		echo "<tr><td colspan='2'><b style='color:#0000ff'>Phone No:&nbsp&nbsp&nbsp&nbsp&nbsp</b></td><td>".$row['phone_no']."</td></tr>";
+		echo "<tr><td colspan='2'><b style='color:#0000ff'>Qualification:&nbsp&nbsp&nbsp&nbsp&nbsp</b></td><td>".$row['qualification']."</td></tr></table>";
+
 		echo "</div><br>";
 	}
 }
